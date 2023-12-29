8753740c2c180824f2585c74b422c284ccec2d02
Lim Geun <shing100@naver.com>
2023-12-29 16:54:55 +0900

    create User

--- app/api/users/auth/route.ts
+++ app/api/users/auth/route.ts
@@ -2,9 +2,56 @@ import { NextResponse, NextRequest } from "next/server";
 import client from "@/libs/server/client";
 
 export const POST = async (req: Request) => {
-    const res = await req.json();
-    console.log(res);
+    const { phone, email } = await req.json();
+    const payload = phone ? { phone: +phone } : { email };
+    const user = await client.user.upsert({
+        where: {
+            ...payload,
+        },
+        create: {
+            name: "Anonymous",
+            ...payload,
+        },
+        update: {},
+    });
+    console.log(user);
+    /* if (email) {
+      user = await client.user.findUnique({
+        where: {
+          email,
+        },
+      });
+      if (user) console.log("found it.");
+      if (!user) {
+        console.log("Did not find. Will create.");
+        user = await client.user.create({
+          data: {
+            name: "Anonymous",
+            email,
+          },
+        });
+      }
+      console.log(user);
+    }
+    if (phone) {
+      user = await client.user.findUnique({
+        where: {
+          phone: +phone,
+        },
+      });
+      if (user) console.log("found it.");
+      if (!user) {
+        console.log("Did not find. Will create.");
+        user = await client.user.create({
+          data: {
+            name: "Anonymous",
+            phone: +phone,
+          },
+        });
+      }
+      console.log(user);
+    } */
     return NextResponse.json({
-        res
+        user
     });
 };
