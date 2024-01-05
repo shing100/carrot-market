21730d3a035f3c013a644a2d3943f97c9ed70e70
Lim Geun <shing100@naver.com>
2024-01-05 13:16:10 +0900

    [20240105] 상품 리스트

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -2,6 +2,15 @@ import client from "@/libs/server/client";
 import {NextResponse} from "next/server";
 import authHandler from "@/libs/server/authHandler";
 
+export const GET = authHandler(async (req: Request, res: Response) => {
+    const products = await client.product.findMany({});
+    return NextResponse.json({
+        ok: true,
+        products,
+    });
+});
+
+
 export const POST = authHandler(async (req: Request, res: Response) => {
     const { session: { user }} = req;
     const { name, price, description } = await req.json();
